34d788e94ec1147483c05cf922c34f014d28618e
c huseby <catherine_huseby@outlook.com>
2023-11-13 09:56:48 -0600

    Initial Commit

---
+++ flashcardapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flashcardapp
+//
+//  Created by CATHERINE HUSEBY on 11/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
