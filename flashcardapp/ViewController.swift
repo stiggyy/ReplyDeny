a326f44fbb51979fdd5b0093bac53d780d5eb9b5
Corey Atkinson <catkinson2025@gmail.com>
2023-11-14 09:31:53 -0600

    help

--- flashcardapp/ViewController.swift
+++ flashcardapp/ViewController.swift
@@ -6,34 +6,28 @@
 //
 
 import UIKit
-class AppData: Encodable
+class AppData
 {
 
     
     
     
 }
-class card: Codable
+class card
 {
-    var question = ""
-    var answer = ""
+    var question : String
+    var answer : String
     
     
-}
-
-class cardSetClass: Codable {
-    
-    var setName = ""
-    var setColor = ""
-    var setImage = ""
-    
-    init(setName: String, setColor: String) {
-        self.setName = setName
-        self.setColor = setColor
+    init(question2: String, answer2: String) {
+        question = question2
+        answer = answer2
+    }
+   
     }
     
+    
 }
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
